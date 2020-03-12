7b5c13dfbc41267bacad01e201312c2ec5c60057
wags83 <gpwagner@gmail.com>
2020-03-12 17:15:35 -0400

    donzo

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 /***** Deliverable 1 *****/
-const header = document.querySelector("#header")
-console.log("Here's your header:", header)
+document.addEventListener("DOMContentLoaded", function() {
+  const header = document.querySelector("#header");
+  const container = document.getElementsByClassName("player-container")[0];
+  console.log("Here's your header:", header);
 
+  /***** Deliverable 2 *****/
 
-/***** Deliverable 2 *****/
+  header.innerHTML = `<h1 id="header" style="color: red;">Welcome to Ian's Liverpool FC Fan Club</h1>`;
 
+  /***** Deliverable 3 *****/
+  console.log("PLAYERS array looks like this:", PLAYERS);
 
-/***** Deliverable 3 *****/
-// console.log("PLAYERS array looks like this:", PLAYERS)
+  PLAYERS.forEach(element => {
+    let playerDiv = document.createElement("div");
+    playerDiv.innerHTML = `
+        <div class="player" data-number="${element.name}">
+        <h3>
+            ${element.name} (<em>${element.nickname}</em>)
+        </h3>
+        <img src="${element.photo}" alt="${element.name}">
+        </div>`;
+    container.appendChild(playerDiv);
+  });
+  /***** Deliverable 4 *****/
 
-
-/***** Deliverable 4 *****/
+  document.getElementsByTagName("div")[10].remove();
+});
